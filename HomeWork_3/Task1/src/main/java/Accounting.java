2acd3835e8c67fe3b14379d24ad34463dd2be923
Valentina <vansimova@yandex.ru>
2023-09-29 12:30:36 +0300

    Task1 solved

--- HomeWork_3/Task1/src/main/java/Accounting.java
+++ HomeWork_3/Task1/src/main/java/Accounting.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Accounting {
+import java.util.List;
+
+public class Accounting {
+    private Employee employee;
+    private int baseSalary;
+
+    public Accounting(Employee employee, int baseSalary) {
+        this.employee = employee;
+        this.baseSalary=baseSalary;
+    }
+
+    public int calculateNetSalary() {
+        int tax = (int) (baseSalary * 0.25);//calculate in otherway
+        return baseSalary - tax;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
 }
--- HomeWork_3/Task1/src/main/java/Employee.java
+++ HomeWork_3/Task1/src/main/java/Employee.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.Date;
+//1) Переписать код в соответствии с Single Responsibility Principle:
+// Подсказка: вынесите метод calculateNetSalary() в отдельный класс
+public class Employee {
+
+    private String name;
+    private Date dob;
+
+    public Employee(String name, Date dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public String getEmpInfo() {
+
+        return "name - " + name + " , dob - " + dob.toString();
+    }
+
 }
