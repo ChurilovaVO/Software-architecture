333e7922849c5bb9371b008b10e4cf3aefb165c5
Valentina <vansimova@yandex.ru>
2023-09-29 12:37:09 +0300

    Task4 solved

--- HomeWork_3/Task4/src/main/java/Rectangle.java
+++ HomeWork_3/Task4/src/main/java/Rectangle.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Rectangle {
+//4) Переписать код в соответствии с Liskov Substitution Principle:
+public class Rectangle implements Shape{
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    @Override
+    public int area() {
+        return this.height*this.width;
+    }
 }
--- HomeWork_3/Task4/src/main/java/Square.java
+++ HomeWork_3/Task4/src/main/java/Square.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Square {
+public class Square implements Shape {
+   private int side;
+
+   public Square(int side) {
+        this.side = side;
+   }
+    public void setSide(int side) {
+        this.side = side;
+    }
+    @Override
+    public int area() {
+        return this.side*this.side;
+    }
 }
